6b415d0dd2050fbf47fbb4cae3ed74126aebdc2e
Takashi Abe <tabe@salesforce.com>
2017-06-16 12:12:49 +0900

    erase root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :accounts
-  root  "accounts#index"
+  # root  "accounts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
